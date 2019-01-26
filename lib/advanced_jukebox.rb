0ffd0622dd57662d33cfebee32c6b7ddd134a081
yong liang <yongliang24@gmail.com>
2019-01-26 03:59:57 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -61,30 +61,22 @@ def exit_jukebox
    puts "Goodbye"
 end
 
-def run(my_songs)
+def run(title)
   #this method is the same as in jukebox.rb
   help
-  puts "Please enter a command:"
-  user_response = gets.chomp
-  while user_response != "exit"
-    case user_response
-      when "help"
-        help
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      when "list"
-        list(my_songs)
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      when "play"
-        play(my_songs)
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      when "exit"
-        exit_jukebox
-      else
-        puts "Invalid input, please try again:"
-        user_response = gets.chomp
-      end
+  loop do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when 'play'
+      play(title)
+    when 'help'
+      help
+    when 'list'
+      list(title)
+    when 'exit'
+      break
     end
+  end
+  exit_jukebox
 end
