4315d9b1f79859d469b23a4712ddf50c699b582c
yong liang <yongliang24@gmail.com>
2019-01-26 04:11:53 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -49,25 +49,18 @@ end
 #play method, type exit to stop the program
 def run(songs)
   help
-  puts "Please enter a command:"
-  user_response = gets.chomp
-  while user_response != "exit"
-    case user_response
-      when "help"
-        help
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      when "list"
-        list(songs)
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      when "play"
-        play(songs)
-        puts "Please enter a command:"
-        user_response = gets.chomp
-      else
-        puts "Invalid input, please try again"
-        user_response = gets.chomp
+  loop do
+    puts "Please enter a command:"
+    input = gets.chomp
+    case input
+    when 'play'
+      play(songs)
+    when 'help'
+      help
+    when 'list'
+      list(songs)
+    when 'exit'
+      break
     end
   end
   exit_jukebox
