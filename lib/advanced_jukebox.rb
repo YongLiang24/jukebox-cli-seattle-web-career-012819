01b8da79cfc2b2c9c9b0fc3e116b646b43b82837
yong liang <yongliang24@gmail.com>
2019-01-26 03:56:09 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -26,6 +26,9 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and
   #list the songs by name
+  my_songs.each { |song, directory|
+    puts song
+    }
 end
 
 
